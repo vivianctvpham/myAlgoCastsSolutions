0c35c1d5315d379a094d1e7a0c43da11c309849b
vivianP <vivianctvpham@gmail.com>
2020-07-31 18:00:30 -0700

    Another solution to fib algorithm

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -12,4 +12,11 @@ function fib(n) {
   return n < 2 ? n : fib(n - 1) + fib(n - 2);
 }
 
+// function fib(n) {
+//   //What if there was an edge case of -1 nums?
+//   if (n < 0) throw new Error("Cannot be negative");
+
+//   return n < 2 ? n : fib(n - 1) + fib(n - 2);
+// }
+
 module.exports = fib;
