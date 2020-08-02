8c9ada30c1af9df15645f688d3820ccac9b76b4d
vivianP <vivianctvpham@gmail.com>
2020-08-02 10:59:06 -0700

    completed circular algorithm with explanations

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -13,13 +13,21 @@
 //   circular(l) // true
 
 function circular(list) {
+  // Have to trackers running through the list
   let slow = list.getFirst();
   let fast = list.getFirst();
 
-  while (fast.next && fast.next.next) {
+  //As long as fast or fast.next exist, keep running through
+  //the list til the end, if its a singly linked list
+  //it will reach the end and break out of the loop
+  while (fast && fast.next) {
+    //increment the slow by 1 and fast by 2
     slow = slow.next;
     fast = fast.next.next;
 
+    //If at any point in the loop, slow === fast,
+    //we have a cycle and it will return true, since fast lapped over
+    //slow
     if (slow === fast) {
       return true;
     }
